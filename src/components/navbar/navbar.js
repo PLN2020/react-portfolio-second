94dbff9580dd03dd6b17d9cdbcb09a8bfa5e4f95
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2021-10-30 23:18:54 -0400

    additional styling for the scrolling navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 
 function Navbar() {
@@ -18,11 +18,11 @@ function Navbar() {
     return (
         <div className={navbar ? 'navbar active' : 'navbar'}>
             <div className="max-width">
-                <div className="logo"><a href="#">Port<span>folio.</span></a></div>
+                <div className="logo"><a href="#about">Port<span>folio.</span></a></div>
                 <ul className="menu">
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Projects</a></li>
+                    {/* <li><a href="#">About</a></li> */}
                     <li><a href="#">Skills</a></li>
+                    <li><a href="#">Projects</a></li>
                     <li><a href="#">Education</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
