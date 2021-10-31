cd576b5979817199b9b1607be6324d41c8ea704f
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2021-10-31 06:12:48 -0400

    project-data created, not connected to projects component yet

---
+++ src/data/project-data.js
@@ -0,0 +1,17 @@
+export const ProjectData = [
+    {
+        id: 1,
+        title: "This Website",
+        details: "My personal portfolio website built using ReactJS.",
+        tools: [
+            <i class="devicon-firebase-plain-wordmark"></i>,
+            <i class="devicon-react-original-wordmark"></i>,
+            <i class="devicon-html5-plain-wordmark"></i>,
+            <i class="devicon-css3-plain-wordmark"></i>,
+            <i class="devicon-javascript-plain"></i>
+        ],
+        url: "",
+        github: "https://github.com/PLN2020/react-portfolio-second",
+        image: "https://res.cloudinary.com/ditoikfqn/image/upload/v1635653538/my-react-portfolio/portfolio-website.png",
+    },
+]
