a39bebd1ec4b059b61c7a9296056499f4c326ebc
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2021-11-01 04:05:48 -0400

    created files for education section, and data sheets

---
+++ src/data/education-data.js
@@ -0,0 +1,16 @@
+export const EducationData = [
+    {
+        school: "Le Wagon",
+        subject: "Web Development Bootcamp",
+        dateJoining: "October 2020",
+        dateEnd: "2021",
+        description: "Attended the 24 week part-time Web Development bootcamp as part of batch #517, and learned HTML, CSS, Bootstrap, Javascript, SQL, git, GitHub, Heroku and Ruby on Rails.",
+    },
+    {
+        company: "Université de Montréal",
+        designation: "East Asian Studies",
+        dateJoining: "2014",
+        dateEnd: "2019",
+        description: "Completed my Bachelor of Arts in Asian Studies at the University of Montreal. I specialized in Japanese culture and language and participated in a 1 semester exchange program at the University of Kanazawa in 2016.",
+    }
+]
