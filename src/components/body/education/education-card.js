fce5e1dcfac956b91cc17214308915764632d924
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2021-11-01 04:40:10 -0400

    built skeleton for the education component

---
+++ src/components/body/education/education-card.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./education-card.css"
+
+function EducationCard({item}) {
+    return (
+        <div className="education-card">
+            <div className="education-info">
+                
+                <div className="education-school-year">
+                    <h2 className="school-name">{item.school}</h2>
+                    <span> / </span>
+                    <span className="year">{item.dateEnd}</span>
+                </div>
+
+                <div className="education-desc">
+                    <p>{item.description}</p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default EducationCard
---
+++ src/data/other-skills-data.js
@@ -0,0 +1,14 @@
+export const OtherSkillsData = [
+    {
+        title: "JLPT N2",
+        description: "",
+    },
+    {
+        title: "Photography",
+        description: "",
+    },
+    {
+        title: "Cook",
+        description: "",
+    }
+]
