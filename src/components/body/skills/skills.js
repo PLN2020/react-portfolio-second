a8466c3f09c7c98d0bc6c4018aca6b12ae0fe143
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2021-11-28 22:31:41 -0500

    changed navbar to have a permanent background color

--- src/components/body/skills/skills.js
+++ src/components/body/skills/skills.js
@@ -34,7 +34,9 @@ function Skills() {
                     )
                 })}
 
-            </div> 
+            </div>
+            
+             
         </div>
     )
 }
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,25 +2,11 @@ import React, { useState } from 'react'
 import './navbar.css'
 
 function Navbar() {
-    const [navbar,setNavbar] = useState(false);
-
-    const changeBackground = () => {
-        // console.log(window.scrollY);
-        if(window.scrollY >= 200) {
-            setNavbar(true);
-        } else {
-            setNavbar(false);
-        }
-    };
-
-    window.addEventListener('scroll', changeBackground);
-
     return (
-        <div className={navbar ? 'navbar active' : 'navbar'}>
+        <div className="navbar">
             <div className="max-width">
                 <div className="logo"><a href="#about">Portfolio.</a></div>
                 <ul className="menu">
-                    {/* <li><a href="#">About</a></li> */}
                     <li><a href="#skills">Skills</a></li>
                     <li><a href="#projects">Projects</a></li>
                     <li><a href="#education">Education</a></li>
