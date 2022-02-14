2f3baa620f141808129a70aefe6052b982e2242c
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2022-02-14 03:32:41 -0500

    fixed skills section container

--- src/components/body/skills/skills.js
+++ src/components/body/skills/skills.js
@@ -8,35 +8,30 @@ function Skills() {
 
     return (
         <div className="skills">
-            <div className="skills-container">
-                
-                {data.map((item) => {
-                    return (
-                        <div className="skills-card">
-                            <div className="icon-wrapper section">
-                                <span className="icon">{item.icon}</span>
-                                <span className="title">{item.type}</span>
-                            </div>
+            {data.map((item) => {
+                return (
+                    <div className="skills-card">
+                        <div className="icon-wrapper section">
+                            <span className="icon">{item.icon}</span>
+                            <span className="title">{item.type}</span>
+                        </div>
 
-                            <div className="list-wrapper section">
-                                {item.list.map((skill) => {
-                                    return (
-                                        <ul className="skills-list">
-                                            <li className="skill-item">
-                                                <span className="skill-icon">{skill.icon}</span>
-                                                <span className="skill-name">{skill.name}</span>
-                                            </li>
-                                        </ul>
-                                    )
-                                })}
-                            </div>
-                        </div> 
-                    )
-                })}
-
-            </div>
+                        <div className="list-wrapper section">
+                            {item.list.map((skill) => {
+                                return (
+                                    <ul className="skills-list">
+                                        <li className="skill-item">
+                                            <span className="skill-icon">{skill.icon}</span>
+                                            <span className="skill-name">{skill.name}</span>
+                                        </li>
+                                    </ul>
+                                )
+                            })}
+                        </div>
+                    </div> 
+                )
+            })}
             
-             
         </div>
     )
 }
